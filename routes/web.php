99ef419bfd4bc610822a212dbbae1b4694d9058d
sewebcourse <sewebcourse@gmail.com>
2022-03-01 21:39:19 +0600

    test

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/gallery', function () {
-    return view('gallery');
-});
